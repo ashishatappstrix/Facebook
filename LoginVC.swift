ff89deb1832a39f932fa2a614817cef0d6eeff05
Ashish Shaik <ashish.shaik@sapientrazorfish.com>
2018-11-16 02:33:07 -0600

    Login screen bottom section UI Setup

--- LoginVC.swift
+++ LoginVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var textFieldsView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +19,10 @@ class LoginVC: UIViewController {
     }
 
     override func viewDidLayoutSubviews() {
+        //Configure View Elements
         configureTextFieldsView()
+        configureLoginButton()
+        configureRegisterButton()
     }
     
     //Configures appearance of textview
@@ -45,7 +50,24 @@ class LoginVC: UIViewController {
         //corner radius
         textFieldsView.layer.cornerRadius = 5
         textFieldsView.layer.masksToBounds = true
+    }
+    
+    //Configure Login Button Appearance
+    func configureLoginButton() {
+        loginButton.layer.cornerRadius = 5
+        loginButton.layer.masksToBounds = true
+        loginButton.isEnabled = false
+    }
+    
+    //Configure Register Button
+    func configureRegisterButton() {
+        let border = CALayer()
+        border.borderWidth = 2
+        border.borderColor = UIColor(red: 68/255, green: 105/255, blue: 176/255, alpha: 0.5).cgColor
+        border.frame = CGRect(x: 0, y: 0, width: registerButton.frame.width, height: registerButton.frame.height)
+        registerButton.layer.addSublayer(border)
         
-        
+        registerButton.layer.cornerRadius = 5
+        registerButton.layer.masksToBounds = true
     }
 }
