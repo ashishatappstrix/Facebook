609db9c5e5e4a37f2893e3c0fe49eb277616d354
Ashish Shaik <ashish.shaik@sapientrazorfish.com>
2018-11-16 03:48:03 -0600

    Login Page completion
    Login Page completion

--- LoginVC.swift
+++ LoginVC.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    //UI Objects
+    @IBOutlet weak var handsImageView: UIImageView!
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var textFieldsView: UIView!
+    
+    //Constraints
+    @IBOutlet weak var coverImageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var registerButtonBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var handsImageTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var whiteImageYConstraint: NSLayoutConstraint!
+    
+    //MARK: View Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Notification while displaying/hiding keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: Notification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
     }
 
     override func viewDidLayoutSubviews() {
@@ -25,6 +47,50 @@ class LoginVC: UIViewController {
         configureRegisterButton()
     }
     
+    //MARK: Keyboard Show/Hide Implementation
+    
+    //Actions while showing keyboard
+    @objc func keyboardWillShow(notification: Notification) {
+        //Reduce size by 75px
+        coverImageTopConstraint.constant -= 75
+        handsImageTopConstraint.constant -= 75
+        whiteImageYConstraint.constant += 50
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            print("Begin: \(keyboardSize.height)")
+            registerButtonBottomConstraint.constant += keyboardSize.height
+        }
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            guard let `self` = self else { return }
+            self.handsImageView.alpha = 0
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //Actions while hiding keyboard
+    @objc func keyboardWillHide(notification: Notification) {
+        //Increase size by 75px
+        coverImageTopConstraint.constant += 75
+        handsImageTopConstraint.constant += 75
+        whiteImageYConstraint.constant -= 50
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            print("End: \(keyboardSize.height)")
+            registerButtonBottomConstraint.constant -= keyboardSize.height
+        }
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            guard let `self` = self else { return }
+            self.handsImageView.alpha = 1
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //Hide keyboard when tapped on white space
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+    //MARK: UI Configuration
     //Configures appearance of textview
     func configureTextFieldsView() {
         let border = CALayer()
