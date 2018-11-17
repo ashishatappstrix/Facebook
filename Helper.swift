4aba1e49108da29765f8ef2fe1f2bfd25821d661
Ashish Shaik <ashish.shaik@sapientrazorfish.com>
2018-11-16 22:10:55 -0600

    Partial Registration page Implementation

---
+++ Helper.swift
@@ -0,0 +1,35 @@
+//
+//  Helper.swift
+//  FaceBook
+//
+//  Created by Ashish Shaik on 11/16/18.
+//  Copyright © 2018 Ashish Shaik. All rights reserved.
+//
+
+import Foundation
+
+class Helper {
+
+    // validate email address function / logic
+    func isValid(email: String) -> Bool {
+        
+        // declaring the rule of regular expression (chars to be used). Applying the rele to current state. Verifying the result (email = rule)
+        let regex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        let result = test.evaluate(with: email)
+        
+        return result
+    }
+    
+    
+    // validate name function / logic
+    func isValid(name: String) -> Bool {
+        
+        // declaring the rule of regular expression (chars to be used). Applying the rele to current state. Verifying the result (email = rule)
+        let regex = "[A-Za-z]{2,}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        let result = test.evaluate(with: name)
+        
+        return result
+    }
+}
--- RegisterVC.swift
+++ RegisterVC.swift
@@ -12,27 +12,158 @@ class RegisterVC: UIViewController {
 
     @IBOutlet weak var contentView_width: NSLayoutConstraint!
     @IBOutlet weak var contentSubviews_width: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        contentView_width.constant = self.view.frame.width * 6
+        contentView_width.constant = self.view.frame.width * 7
         contentSubviews_width.constant = self.view.frame.width
+        setFieldsCornerRadius()
+        
+        //Set Delegates
+    
+        emailOrMobileTextField.addTarget(self, action: #selector(RegisterVC.emailTextFieldDidChange(_:)),
+                            for: UIControlEvents.editingChanged)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        addButtonBorders(for: [maleImageButton, femaleImageButton])
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func alreadyHaveAccountTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    func setFieldsCornerRadius() {
+        setCornerRadius(for: getStartedButton)
+        setCornerRadius(for: firstNameTextField)
+        setCornerRadius(for: surNameTextField)
+        setCornerRadius(for: nameContinueButton)
+        setCornerRadius(for: bdayContinueButton)
+        setCornerRadius(for: emailOrMobileTextField)
+        setCornerRadius(for: emailAddressButton)
+        setCornerRadius(for: passwordTextField)
+        setCornerRadius(for: signupButton)
+    }
+    
+    func setCornerRadius(for view:UIView) {
+        view.layer.cornerRadius = 5
+        view.layer.masksToBounds = true
+    }
+    
+    func addButtonBorders(for buttons: [UIButton]) {
+        
+        for button in buttons {
+            let border = CALayer()
+            border.borderWidth = 2
+            border.borderColor = UIColor.lightGray.cgColor
+            border.frame = CGRect(x: 0, y: 0, width: button.frame.width, height: button.frame.height)
+            button.layer.addSublayer(border)
+            button.layer.cornerRadius = 5
+            button.layer.masksToBounds = true
+        }
+    }
+    
+    func setEmailContinueButton() {
+        emailOrLabelButton.setTitle("Continue", for: .normal)
+        emailOrLabelButton.backgroundColor = UIColor.blue
+        emailOrLabelButton.setTitleColor(.white, for: .normal)
+    }
+    
+    func setLabelTextForEmailView() {
+        if isButtonTextEmail {
+            emailOrMobileTextLabel.text = "What's your email address?"
+            emailOrLabelButton.setTitle("You'll use this email address when you log in and if you ever need to reset your password", for: .normal)
+            emailAddressButton.setTitle("Use your mobile numer", for: .normal)
+            
+            isButtonTextEmail = false
+            
+        } else {
+            emailOrMobileTextLabel.text = "What's your mobile number?"
+            emailOrLabelButton.setTitle("You'll use this number when you log in and if you ever need to reset your password", for: .normal)
+            emailAddressButton.setTitle("Use your email address", for: .normal)
+            
+            isButtonTextEmail = true
+        }
+    }
+    
+    //MARK: Get Started View - Outlets - Actions
+    
+    @IBOutlet weak var getStartedButton: UIButton!
+    
+    @IBAction func getStartedButton_tapped(_ sender: Any) {
+        
+    }
+    
+    //MARK: Name View - Outlets - Actions
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var surNameTextField: UITextField!
+    @IBOutlet weak var nameContinueButton: UIButton!
+    
+    @IBAction func nameContinueButton_tapped(_ sender: Any) {
+        
+    }
+    
+    //MARK: Bday View - Outlets - Actions
+    
+    @IBOutlet weak var bdayContinueButton: UIButton!
+    
+    @IBAction func bdayContinueButton_Tapped(_ sender: Any) {
+    }
+    
+    //MARK: Gender View - Outlets - Actions
+    
+    @IBOutlet weak var femaleImageButton: UIButton!
+    @IBOutlet weak var maleImageButton: UIButton!
+    
+    @IBAction func maleImageButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func femaleImageButtonTapped(_ sender: Any) {
+    }
+    
+    //MARK: Mobile/Email Number View - Outlets - Actions
+    
+    @IBOutlet weak var emailOrMobileTextLabel: UILabel!
+    @IBOutlet weak var emailOrMobileTextField: UITextField!
+    @IBOutlet weak var emailAddressButton: UIButton!
+    @IBOutlet weak var emailOrLabelButton: UIButton!
+    
+    var isButtonTextEmail = true
+    @IBAction func emailAddressButtonTapped(_ sender: Any) {
+       setLabelTextForEmailView()
+    }
+    
 
+    
+    @IBAction func emailOrLabelButtonTapped(_ sender: Any) {
+        
+        
+    }
+    //MARK: Password View - Outlets - Actions
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //MARK: Password View - Outlets - Actions
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBAction func signupButtonTapped(_ sender: Any) {
+        
+    }
+}
+
+extension RegisterVC {
+    
+    @objc func emailTextFieldDidChange(_ textField: UITextField) {
+        if textField.text?.characters.count != 0 {
+            setEmailContinueButton()
+        } else {
+            self.setLabelTextForEmailView()
+        }
+    }
 }
